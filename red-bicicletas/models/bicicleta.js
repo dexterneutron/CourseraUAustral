b20ba26dc35e5adb66372315d85489741ec0f659
dexterneutron <felixdlugo@gmail.com>
2020-06-09 17:25:45 -0400

    MVC
    MVC Sin BDD

---
+++ red-bicicletas/models/bicicleta.js
@@ -0,0 +1,36 @@
+var Bicicleta=function (id, color,modelo, ubicacion) {
+this.id=id;
+this.color=color;
+this.modelo=modelo;
+this.ubicacion=ubicacion;
+}
+Bicicleta.prototype.toString= function() {
+    return 'id: '+this.id+' |color: '+this.color;
+}
+Bicicleta.allBicis=[];
+Bicicleta.add=function(aBici){
+    Bicicleta.allBicis.push(aBici);
+}
+Bicicleta.findById=function(aBiciId) {
+    var aBiciId=Bicicleta.allBicis.find(x=>x.id==aBiciId);
+    if(aBiciId)
+        return aBiciId;
+    else
+        throw new Error(`No existe bicicleta con el ID ${aBiciId}`);
+
+}
+Bicicleta.removeById=function(aBiciId) {
+    for(var i=0;i<Bicicleta.allBicis.length;i++){
+        if(Bicicleta.allBicis[i].id==aBiciId){
+            Bicicleta.allBicis.splice(i,1);
+            break;
+        }
+    }
+}
+
+
+var a=new Bicicleta(1,'rojo','urbana',[9.739334,  -63.201816])
+var b=new Bicicleta(2,'blanca','urbana',[9.740061,-63.2012796])
+Bicicleta.add(a);
+Bicicleta.add(b);
+module.exports=Bicicleta;
